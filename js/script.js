47bce061696dd66760ff2b6e90f345f0fead4658
Bethany Garcia <bethanygarcia817@gmail.com>
2019-04-01 22:08:40 +0000

    extended

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
 $("#search-button").click(function(){
+var random = Math.floor(Math.random() * 20);
 
  var user_input = $("#search-term").val();
   $.ajax({
@@ -11,8 +11,19 @@ $("#search-button").click(function(){
       method: "GET",  
       success: function(response){
         $("#images").empty("");
-        $("#images").append("<img src= '" + response.data[0].images.original.url + "' ></a>");
+        $("#images").append("<img src= '" + response.data[random].images.original.url + "' ></a>");
       }
   });
 });
-
+$("#random-button").click(function(){
+ var random1 = Math.floor(Math.random() * 50);
+  var random2 = Math.floor(Math.random() * 100);
+ $.ajax({
+  url:"https://api.giphy.com/v1/gifs/search?q=" +random2+ "&rating=pg&api_key=dc6zaTOxFJmzC",
+  method: "GET",
+  success: function(response){
+    $("#images").empty("");
+    $("#images").append("<img src= '" + response.data[random1].images.original.url + "' ></a>");
+  }
+ });
+});
